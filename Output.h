d7e4c225b35e9cdc0bfc0405f3628c02f3e14c90
ibrahimhassan-ihk2005 <ibrahim.ahmed051@eng-st.cu.edu.eg>
2025-11-26 16:46:36 +0200

    Update Output.h

--- Output.h
+++ Output.h
@@ -20,25 +20,28 @@ public:
 
 	window* CreateWind(int wd, int h, int x, int y) const; //Creates user interface window
 
-	
+
 	// Draws 2-input AND gate
 	void DrawAND2(GraphicsInfo r_GfxInfo, bool selected = false) const;
 	void DrawAND3(GraphicsInfo r_GfxInfo, bool selected = false) const;
 	void DrawOR2(GraphicsInfo r_GfxInfo, bool selected = false) const;
 	void DrawOR3(GraphicsInfo r_GfxInfo, bool selected = false) const;
 	void DrawNAND2(GraphicsInfo r_GfxInfo, bool selected = false) const;
-	void DrawNAND3(GraphicsInfo r_GfxInfo, bool selected = false) const;
-	void DrawNOR2(GraphicsInfo r_GfxInfo, bool selected = false) const;
-	void DrawNOR3(GraphicsInfo r_GfxInfo, bool selected = false) const;
-	void DrawXOR2(GraphicsInfo r_GfxInfo, bool selected = false) const;
-	void DrawXNOR2(GraphicsInfo r_GfxInfo, bool selected = false) const;
-	void DrawNOT(GraphicsInfo r_GfxInfo, bool selected = false) const;
 
+	void DrawSwitch(GraphicsInfo r_GfxInfo, bool selected, bool isON) const;
+	
+	void DrawNOR2(GraphicsInfo r_GfxInfo, bool selected = false) const;
+	void DrawXNOR2(GraphicsInfo r_GfxInfo, bool selected = false) const;
+	void DrawXOR2(GraphicsInfo r_GfxInfo, bool selected = false) const;
+	void DrawXOR3(GraphicsInfo r_GfxInfo, bool selected = false) const;
+	
+	void DrawBUFFER(GraphicsInfo r_GfxInfo, bool selected = false) const;
+	void DrawLED(GraphicsInfo r_GfxInfo, bool on, bool selected) const;
 	///TODO: Make similar functions for drawing all other gates, switch, and LED, .. etc
 
 	// Draws Connection
 	void DrawConnection(GraphicsInfo r_GfxInfo, bool selected = false) const;
-	
+
 	void PrintMsg(string msg) const;	//Print a message on Status bar
 
 
